4972cae6a92c214ae1a3384b97e24a5aeeb69cc9
AgustinReyesC <reyescastilloagustinalexis@gmail.com>
2026-06-02 12:22:14 -0600

    auth completa, con register, login y findme, y un middleware de protección de jwt por token

---
+++ backend/src/middlewares/auth.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken')
+const User = require('../models/User')
+
+const protect = async (req, res, next) => {
+    try {
+        const authHeader = req.headers.authorization
+        if(!authHeader || !authHeader.startsWith('Bearer ')) {
+            return res.status(401).json({message: 'No autorizado, falta token'})
+        }
+        const token = authHeader.split(' ')[1]
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        req.user = await User.findById(decoded.id).select('-password')
+        if(!req.user) {
+            return res.status(401).json({message: 'Usuario no encontrado'})
+        }
+        next()
+    } catch (error) {
+        res.status(401).json({ message: 'token inválido'})
+    }
+}
+
+module.exports = { protect }
---
+++ backend/src/models/User.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose')
+const bcrypt = require ('bcryptjs')
+
+// definición del esquema usuario
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'El nombre es obligatorio'],
+        minlength: [3, 'El nombre debe tener al menos 3 caracteres de longitud'],
+        trim: true
+    },
+    email: {
+        type: String,
+        required: [true, 'El email es obligatorio'],
+        unique: true,
+        lowercase: true,
+        trim: true,
+        index: true
+    },
+    password: {
+        type: String,
+        required: [true, 'La contraseña es obligatoria'],
+        minlength: [8, 'La contraseña debe tener al menos 8 caracteres']
+    },
+    //definimos rol de administrador o usuario común
+    role: {
+        type: String,
+        enum: ['user', 'admin'],
+        default: 'user'
+    }
+}, {timestamps: true})
+
+
+
+
+
+userSchema.pre('save', async function(next) {
+    if(!this.isModified('password')) return next()
+    this.password = await bcrypt.hash(this.password, 12)
+    next()
+})
+
+userSchema.methods.comparePassword = async function(candidatePassword) {
+    return await bcrypt.compare(candidatePassword, this.password)
+}
+
+userSchema.methods.toJSON = function() {
+    const user = this.toObject()
+    delete user.password
+    return user
+}
+
+module.exports = mongoose.model('User', userSchema)
