1137d31a716cdcce8568552117e2cdf50264366b
AgustinReyesC <reyescastilloagustinalexis@gmail.com>
2026-06-02 16:22:05 -0600

    feat: refresh token, rotación, logout

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const {register, login, findMe} = require('../controllers/authController')
+const {register, login, findMe, refresh, logout} = require('../controllers/authController')
 const { protect, isAdmin } = require('../middlewares/auth')
 const validate = require('../middlewares/validate')
 const { registerSchema, loginSchema } = require('../validators/authValidator')
@@ -8,5 +8,7 @@ const { registerSchema, loginSchema } = require('../validators/authValidator')
 router.post('/register', validate(registerSchema), register)
 router.post('/login', validate(loginSchema), login)
 router.get('/me', protect, findMe)
+router.post('/refresh', refresh)
+router.post('/logout', protect, logout)
 
 module.exports = router
